ed8eb47d68a03db8d3eadd74f75e99f07de773e6
adam <adam.ondrejkovic@gmail.com>
2021-04-03 18:21:27 +0200

    Finished check authentication and added error handler

--- src/main/java/dal/IAuthentication.java
+++ src/main/java/dal/IAuthentication.java
@@ -1,6 +1,7 @@
 package dal;
 
 public interface IAuthentication {
+    public Object getAuthentication(String email, String password);
     boolean authenticateTeacher(String email, String password);
     boolean authenticateStudent(String email, String password);
 }
---
+++ src/main/java/error/ErrorHandler.java
@@ -0,0 +1,12 @@
+package error;
+
+public class ErrorHandler {
+
+    public void handleErrorUser(String message) {
+        System.out.println(message);
+    }
+
+    public void errorDevelopmentInfo(String message) {
+        System.out.println(message);
+    }
+}
