733a8ffc39c491ac9650ac70ff9dea48477c4afc
Faustas Anulis <45820787+faustis1337@users.noreply.github.com>
2021-04-10 15:21:12 +0200

    students page

--- src/main/java/gui/controller/LoginController.java
+++ src/main/java/gui/controller/LoginController.java
@@ -119,26 +119,26 @@ public class LoginController {
     }
 
     public void goToTeachersView(Stage stage, User user) {
-        session.startSession(user, stage);
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/userpage/TeacherPage.fxml"));
             Parent root = fxmlLoader.load();
             stage.setScene(new Scene(root));
+            session.startSession(user, stage);
 
         } catch (IOException e) {
-            session.stopSession(stage);
+            e.printStackTrace();
         }
     }
 
     public void goToStudentsView(Stage stage, User user) {
-        session.startSession(user, stage);
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/userpage/StudentPage.fxml"));
             Parent root = fxmlLoader.load();
             stage.setScene(new Scene(root));
+            session.startSession(user, stage);
 
         } catch (IOException e) {
-            session.stopSession(stage);
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/gui/controller/TeacherPageController.java
+++ src/main/java/gui/controller/TeacherPageController.java
@@ -1,9 +1,12 @@
 package gui.controller;
 import be.Class;
+import be.Date;
 import be.user.Student;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -14,26 +17,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class TeacherPageController implements Initializable {
-
-    @FXML
-    private TableView<Student> studentListTable;
-    TableColumn<Student, String> colName = new TableColumn<>();
-    TableColumn<Student, String> colSurName = new TableColumn<>();
-    TableColumn<Student, String> colEmail = new TableColumn<>();
-/*
-    TableColumn<Class, String> colClassesNames = new TableColumn<>("All Classes");
-*/
     @FXML
     private ListView<Class> classListTable;
-    TableColumn<Class, String> className = new TableColumn<>();
 
     @FXML
     private Text lblTotalPercentage;
+    @FXML
+    private DatePicker datePicker;
     @FXML
     private Text lblMissedClassCount;
     @FXML
@@ -51,15 +47,6 @@ public class TeacherPageController implements Initializable {
     }
 
     private void setAllStudentList() {
-        studentListTable.getColumns().addAll(colName, colSurName, colEmail);
-        colName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
-        colSurName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
-
-        Student student = new Student("Faustas","Anulis","faustasanulis@gmail.com",1);
-        List<Student> studentList = new ArrayList<>();
-        studentList.add(student);
-        studentListTable.setItems(FXCollections.observableList(studentList));
     }
 
     private void setAllClasses(){
@@ -80,5 +67,12 @@ public class TeacherPageController implements Initializable {
 
     @FXML
     private void btnDeleteClass() {
+    }
+
+    public void submitDate(ActionEvent actionEvent) {
+        int year = datePicker.getValue().getYear();
+        int month = datePicker.getValue().getMonthValue();
+        int day = datePicker.getValue().getDayOfMonth();
+        Date date = new Date(year,month,day);
     }
 }
